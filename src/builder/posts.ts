d5e437af0094d40d28302a76922ce394351f7a6d
Yuichi Mukai <mukai_yuichi@yahoo.co.jp>
2022-06-09 23:26:27 +0900

    Qita_RSSを削除

--- src/builder/posts.ts
+++ src/builder/posts.ts
@@ -1,18 +1,18 @@
-import fs from "fs-extra"
-import Parser from "rss-parser"
+import fs from "fs-extra";
+import Parser from "rss-parser";
 
-export default {}
+export default {};
 
-const parser = new Parser()
+const parser = new Parser();
 
 async function fetchFeedItems(url: string) {
-  const feed = await parser.parseURL(url)
-  let sourceType
+  const feed = await parser.parseURL(url);
+  let sourceType;
   if (url.indexOf("zenn") !== -1) {
-    sourceType = "zenn"
+    sourceType = "zenn";
   }
 
-  if (!feed?.items?.length) return []
+  if (!feed?.items?.length) return [];
 
   // return item which has title and link
   return feed.items
@@ -24,28 +24,28 @@ async function fetchFeedItems(url: string) {
         isoDate,
         dateMiliSeconds: isoDate ? new Date(isoDate).getTime() : 0,
         sourceType,
-      }
+      };
     })
-    .filter(({ title, link }) => title && link)
+    .filter(({ title, link }) => title && link);
 }
 
 async function getFeedItemsFromSources(sources: undefined | string[]) {
-  if (!sources?.length) return []
-  let feedItems = []
+  if (!sources?.length) return [];
+  let feedItems = [];
   try {
     for (const url of sources) {
-      const items = await fetchFeedItems(url)
-      if (items) feedItems = [...feedItems, ...items]
+      const items = await fetchFeedItems(url);
+      if (items) feedItems = [...feedItems, ...items];
     }
-    return feedItems
+    return feedItems;
   } catch (error) {}
 }
 
-const ZENN_RSS = "https://zenn.dev/yuichi_m/feed"
+const ZENN_RSS = "https://zenn.dev/yuichi_m/feed";
 
-;(async function () {
-  const items = (await getFeedItemsFromSources([ZENN_RSS])) ?? []
-  items.sort((a, b) => b.dateMiliSeconds - a.dateMiliSeconds)
-  fs.ensureDirSync(".contents")
-  fs.writeJsonSync(".contents/posts.json", items)
-})()
+(async function () {
+  const items = (await getFeedItemsFromSources([ZENN_RSS])) ?? [];
+  items.sort((a, b) => b.dateMiliSeconds - a.dateMiliSeconds);
+  fs.ensureDirSync(".contents");
+  fs.writeJsonSync(".contents/posts.json", items);
+})();
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,4 @@
-export const API_ENDPOINT = 'https://yuichidev.microcms.io/api/v1';
-export const QITA_RSS = "https://qiita.com/yuichi_mukai/feed.atom";
+export const API_ENDPOINT = "https://yuichidev.microcms.io/api/v1";
 export const ZENN_RSS = "https://zenn.dev/yuichi_m/feed";
 export const ARTICLES_PER_PAGE = 6;
 export const DEAFULT_HEADER_HEIGHT = 103;
